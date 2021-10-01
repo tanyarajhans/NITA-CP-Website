8bbd49adccf085c1c8e3b53922ad7eb66f6bc501
Atithi Kumar Singh <mails4rockstararyan@gmail.com>
2021-10-01 23:28:56 +0530

    initial commit

---
+++ nita_cp/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import 'tailwindcss/tailwind.css'
+
+function Navbar() {
+    return (
+        <nav className="flex flex-row justify-between mx-10">
+            <div className="text-3xl m-2">
+                NITA-CP
+            </div>
+            <div>
+                
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
---
+++ nita_cp/pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Navbar from "../components/Navbar";
+
+export default function Home() {
+  return (
+    <div className="home">
+      {/* Navbar */}
+      <Navbar />
+    </div>
+  );
+}
