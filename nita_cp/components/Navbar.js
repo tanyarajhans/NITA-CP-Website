33d90067486730ace963b8b20ff72b1c18a7c134
Atithi Kumar Singh <mails4rockstararyan@gmail.com>
2021-10-01 23:52:54 +0530

    initial commit

--- nita_cp/components/Navbar.js
+++ nita_cp/components/Navbar.js
@@ -1,17 +1,32 @@
-import React from 'react'
-import 'tailwindcss/tailwind.css'
+import React from "react";
+import "tailwindcss/tailwind.css";
+import Link from 'next/link'
 
 function Navbar() {
-    return (
-        <nav className="flex flex-row justify-between mx-10">
-            <div className="text-3xl m-2">
-                NITA-CP
-            </div>
-            <div>
-                
-            </div>
-        </nav>
-    )
+  return (
+    <nav className="flex flex-row justify-between mx-10 align-center">
+      <div className="text-3xl m-2">NITA-CP</div>
+      <div>
+        <ul className="flex justify-between m-2">
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a>About Us</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/blog/hello-world">
+              <a>Blog Post</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
