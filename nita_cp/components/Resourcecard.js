2db5b2f9d6eeb599eefb4ea2184b99056b09c9c0
Atithi Kumar Singh <mails4rockstararyan@gmail.com>
2021-10-02 20:03:18 +0530

    created card

---
+++ nita_cp/components/Resourcecard.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Image from "next/image";
+function Resourcecard(props) {
+  return (
+    <div className="flex flex-col w-80 justify-center items-center z-0 m-4 p-3">
+      <Image
+        src={props.Image}
+        alt="Picture of the author"
+        width={320}
+        height={200}
+        className="rounded-xl "
+      />
+      <div className="flex flex-col items-center w-60 -mt-9 z-10 p-2 rounded-md  bg-white">
+          <h1 className=" text-xl font-bold ">{props.name}</h1>
+        <div className="flex w-60 justify-between rounded-md p-2 bg-gray-300  ">
+            <h1>
+                {props.author}
+            </h1>
+            <h1>
+                {props.difficulty}
+            </h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Resourcecard;
--- nita_cp/pages/index.js
+++ nita_cp/pages/index.js
@@ -2,12 +2,19 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Navbar from "../components/Navbar";
+import Resourcecard from "../components/Resourcecard";
 
 export default function Home() {
   return (
     <div className="home">
       {/* Navbar */}
       <Navbar />
+      <Resourcecard
+          Image="https://images.unsplash.com/photo-1539185441755-769473a23570?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1951&q=80"
+          name="STL"
+          author="@striver"
+          difficulty="Intermediate"
+      />
     </div>
   );
 }
