1d36f631f37000bdeac33f072801d12d1259c4af
Atithi Kumar Singh <mails4rockstararyan@gmail.com>
2021-10-08 11:36:32 +0530

    added a contribution page

---
+++ pages/contribute.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+function Contribute() {
+    return (
+        <div className="home">
+            <Navbar></Navbar>
+            <h1>This is contribute page</h1>
+        </div>
+    )
+}
+
+export default Contribute
